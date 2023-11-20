0597a8d727e7712dc7bb593d87869cda60512d4b
GentIeman <ilyashepelev23@gmail.com>
2023-11-20 12:48:48 +0300

    init

---
+++ index.js
@@ -0,0 +1,59 @@
+module.exports = {
+    root: true,
+
+    globals: {
+        MyGlobal: true
+    },
+
+    env: {
+        es6: true,
+        browser: true,
+        node: true
+    },
+
+    extends: [
+        "@vue/eslint-config-typescript",
+        "plugin:vue/vue3-strongly-recommended"
+    ],
+
+    rules: {
+        semi: ["error", "never"],
+        "no-nested-ternary": "off",
+        "vue/block-order": ["error", {
+            "order": [["template", "script"], "style"]
+        }],
+        "vue/no-mutating-props": ["error", {
+            "shallowOnly": false
+        }],
+        "vue/block-lang": ["error",
+            {
+                "script": {
+                    "lang": "ts"
+                }
+            }
+        ],
+        "vue/component-api-style": ["error",
+            ["script-setup", "composition"]
+        ],
+        "vue/html-button-has-type": ["error", {
+            "button": true,
+            "submit": true,
+            "reset": true
+        }],
+        "vue/html-comment-content-spacing": ["error", "always", { "exceptions": ["*"] }],
+        "vue/no-undef-components": ["error"],
+        "vue/no-undef-properties": ["error"],
+        "vue/no-unsupported-features": ["error", {
+            "version": "^2.7.0"
+        }]
+    },
+
+    parserOptions: {
+        parser: require.resolve("@typescript-eslint/parser"),
+        extraFileExtensions: [".vue"],
+        ecmaVersion: "latest",
+        sourceType: "module"
+    },
+
+    plugins: ["@typescript-eslint"]
+}
